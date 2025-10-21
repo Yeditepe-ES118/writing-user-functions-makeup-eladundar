ec163ec67703e05f32fc3dec83df5a404809f17d
eladundar <ela.dundar@std.yeditepe.edu.tr>
2025-10-21 15:18:48 +0300

    Add files via upload

--- lab4.py
+++ lab4.py
@@ -16,6 +16,6 @@ def throw_rock (m, v0, theta):
           "Maximum height is %10.1e m\n" \
           "The speed at maximum height is %10.1e m/s\n" \
           "Kinetic energy at the maximum height is %8.2e J" % (m, v0, theta*180/np.pi, tf, R, hm, vh, Kh))
-        return tf, R, hm, vh, Kh
+    return tf, hm, R, vh, Kh
     
     myresult = throw_rock(1.5, 0.3, 35.20)
